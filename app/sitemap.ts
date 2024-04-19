2f125c5b0f9be4ba5a15b1e884d7711253ee3ae1
Yaroslav Taranich <YaroslavTaranich@yandex.ru>
2024-04-19 15:12:30 +0300

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,57 @@
+import { ICategory, getAllCategories, getAllTools } from "@/services/api";
+import { MetadataRoute } from "next";
+
+const HOST = process.env.HOST || "";
+
+interface ICategoryById {
+  [id: string]: ICategory;
+}
+
+export default async function sitemap() {
+  const categories = await getAllCategories();
+  const categoriesById = categories.reduce((acc: ICategoryById, current) => {
+    return {
+      ...acc,
+      [current.id]: current,
+    } as ICategoryById;
+  }, {});
+  const tools = await getAllTools();
+
+  const map: MetadataRoute.Sitemap = [
+    {
+      url: HOST,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: HOST + "delivery",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+  ];
+
+  categories.forEach((category) => {
+    map.push({
+      url: HOST + category.name,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    });
+  });
+
+  tools.forEach((tool) => {
+    if (tool.categoryId in categoriesById) {
+      const categoryName = categoriesById[tool.categoryId].name;
+      map.push({
+        url: HOST + categoryName + "/" + tool.name,
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.85,
+      });
+    }
+  });
+
+  return map;
+}
