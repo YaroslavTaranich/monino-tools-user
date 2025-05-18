ef57e55c0183c299965e8021e3dbd55cd44e4444
Yaroslav Taranich <YaroslavTaranich@yandex.ru>
2025-05-18 22:02:26 +0300

    add revalidate home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,19 @@ import { Benefits } from '@/components/benefits';
 import { AllCategoriesList } from '@/components/all-categories-list';
 import { DeliverySection } from '@/components/delivery-section';
 
+export async function generateStaticParams() {
+  try {
+    const categories = await getAllCategories();
+
+    return categories;
+  } catch (e) {
+    return [];
+  }
+}
+
+export const revalidate = 360;
+export const dynamic = 'force-dynamic'; // Важно!
+
 export default async function Home() {
   try {
     const categories = await getAllCategories();
